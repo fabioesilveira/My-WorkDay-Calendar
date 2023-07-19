b80374d013f6bd25b289ae9186082ff1488abdc5
Fabio Silveira <fabiosilveira@Fabios-MacBook-Pro.local>
2023-07-18 21:17:48 -0700

    JS implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,41 @@
-var dayWeek = today.format('[Today is] dddd');
-$('#1a').text(dayWeek);
+
+var today = dayjs();
+$('#weekDay').text(today.format('MMMM D'));
+
+var dayWeek = today.format('dddd,');
+$('#currentDay').text(dayWeek);
 
 var today = dayjs();
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+    var currentHour = dayjs().format("HH")
+    console.log (currentHour)
+    var timeblock = document.querySelectorAll('.time-block')
+    for (let i = 0; i < timeblock.length; i++) {
+      
+      var timeblockid = timeblock[i].id.substring(5)
+      console.log (timeblockid)
+      if (timeblockid==currentHour){
+        timeblock[i].classList.add('present')
+      }
+      if (timeblockid<currentHour){
+        timeblock[i].classList.add('past')
+      }
+      if (timeblockid>currentHour){
+        timeblock[i].classList.add('future')
+      }
+  
+    }
+$ ('.saveBtn').on('click',function(){
+  console.log($(this).siblings('textarea').val())
+  console.log($(this).parent('div').attr('id'))
+  localStorage.setItem($(this).parent('div').attr('id'),$(this).siblings('textarea').val())
+})
+  
+var textblocks =  $('.description')
+for (let i = 0; i < textblocks.length; i++) {
+$(textblocks[i]).val(localStorage.getItem($(textblocks[i]).parent('div').attr('id')))
+  
+}
+        
   });
